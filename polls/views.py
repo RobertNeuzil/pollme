09263ee4f29fcbcf79b83342776b301d736101cd
robert neuzil <robertneuzil@gmail.com>
2018-10-24 17:20:35 -0400

    polls template

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def polls_list(request):
-	return HttpResponse('Polls List')
+	return render(request, 'polls/polls_list.html' )
 
 # Create your views here.
